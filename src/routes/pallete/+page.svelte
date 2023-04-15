deffb4c61f8f0a1cfc94586ea0cbdc65139a3efa
OdoakerQ <kubakowieski5151@gmail.com>
2023-04-15 14:19:16 +0200

    first

---
+++ src/routes/pallete/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { Canvas } from '@threlte/core';
+	import Scene from './Scene.svelte';
+</script>
+
+<section class="w-full">
+	<h1>Test</h1>
+	<div class="w-full">
+		<Canvas>
+			<Scene />
+		</Canvas>
+	</div>
+</section>
---
+++ src/routes/pallete/Scene.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { T } from '@threlte/core';
+</script>
+
+<T.Mesh>
+	<T.BoxGeometry args={[2, 2, 1]} />
+	<T.MeshBasicMaterial color="hotpink" />
+</T.Mesh>
